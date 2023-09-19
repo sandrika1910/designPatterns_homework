31dfb962e9e759ac4e0233f0a40af6a88b155eba
git config <sandro.gagnidze.1@btu.edu.ge>
2023-09-19 18:53:41 +0400

    Add Pages

--- src/main/java/Pages/Clicks.java
+++ src/main/java/Pages/Clicks.java
@@ -1,2 +1,10 @@
-package Pages;public class Clicks {
+package Pages;
+
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class Clicks {
+    public SelenideElement formsCard = $$("div[class='card-body']").get(1),
+    practiceForm = $x("//span[text()='Practice Form']");
 }
--- src/main/java/Pages/FillFormPage.java
+++ src/main/java/Pages/FillFormPage.java
@@ -1,2 +1,13 @@
-package Pages;public class FillFormPage {
+package Pages;
+
+import com.codeborne.selenide.SelenideElement;
+import static com.codeborne.selenide.Selenide.$;
+
+public class FillFormPage {
+    public SelenideElement firstNameItem = $("#firstName"),
+    lastNameItem = $("#lastName"),
+    genderItem = $("label[for='gender-radio-1']"),
+    mobileNumberItem = $("#userNumber"),
+    submit = $("#submit");
+
 }
