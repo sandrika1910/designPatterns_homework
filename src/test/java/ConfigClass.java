76b9cfe5390de019eaa82ff94de4af732ec6b0b5
git config <sandro.gagnidze.1@btu.edu.ge>
2023-09-19 18:55:23 +0400

    Add ClickOnButtons.java and ConfigClass.java files

--- src/test/java/ConfigClass.java
+++ src/test/java/ConfigClass.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class ConfigClass {
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class ConfigClass {
+
+    public void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+        Configuration.browserCapabilities = options;
+        Configuration.browserSize = null;
+        Configuration.timeout = 20000;
+        Configuration.baseUrl = "https://demoqa.com/";
+        Configuration.reopenBrowserOnFail = true;
+    }
+
+    public void tearDown() {
+        Selenide.closeWebDriver();
+    }
 }
