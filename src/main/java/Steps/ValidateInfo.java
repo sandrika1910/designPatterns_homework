0dab0c34de192a816bd3f37ca50c4816bab8d384
git config <sandro.gagnidze.1@btu.edu.ge>
2023-09-19 19:29:23 +0400

    Update ValidateInfo.java file

--- src/main/java/Steps/ValidateInfo.java
+++ src/main/java/Steps/ValidateInfo.java
@@ -1,6 +1,7 @@
 package Steps;
 
 import Data.FormData;
+import Pages.FillFormPage;
 import Pages.WindowPopup;
 import com.codeborne.selenide.SelenideElement;
 import io.qameta.allure.Step;
@@ -9,6 +10,7 @@ import org.testng.asserts.SoftAssert;
 public class ValidateInfo {
     FormData formData = new FormData();
     WindowPopup windowPopup = new WindowPopup();
+    FillFormPage fillFormPage = new FillFormPage();
     @Step
     public ValidateInfo validateInfo() {
         int counter = 0;
@@ -25,19 +27,22 @@ public class ValidateInfo {
 
         String studentName = null;
         String mobileNumber = null;
-
+        String gender = null;
         for(int i = 0; i < windowPopup.tableData.size(); i++) {
             SelenideElement tr = windowPopup.tableData.get(i);
             if (tr.$$("td").get(labelIndex).getText().equals("Student Name")) {
                 studentName = tr.$$("td").get(valuesIndex).getText();
             } else if (tr.$$("td").get(labelIndex).getText().equals("Mobile")) {
                 mobileNumber = tr.$$("td").get(valuesIndex).getText();
+            } else if (tr.$$("td").get(labelIndex).getText().equals("Gender")) {
+                gender = tr.$$("td").get(valuesIndex).getText();
             }
 
         }
         SoftAssert softAssert = new SoftAssert();
         softAssert.assertEquals(studentName, formData.firstName + " " + formData.lastName, "Student Name is not as expected in table!");
         softAssert.assertEquals(mobileNumber,formData.mobileNumber, "Mobile Number is not as expected in table!");
+        softAssert.assertEquals(gender,formData.gender, "Gender is not as expected in table!");
         return this;
     }
 
